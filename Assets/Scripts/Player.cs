f5a3711e281139c857bab3e01f84a8fca59adc4d
SSuros <90672862+SSuros@users.noreply.github.com>
2022-02-15 01:08:24 -0500

    Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	
+	public float moveSpeed = 10.0f; 
+
+	public Rigidbody2D player;
+
+	
+	void Start()
+	{
+		player = this.GetComponent<Rigidbody2D>();
+	}
+
+	
+	void FixedUpdate()
+	{
+		
+
+		MovePlayer();
+
+	}
+	
+	public void MovePlayer()
+	{
+		
+		player.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed;
+
+	}
+}
